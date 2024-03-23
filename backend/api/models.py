20deef005147c87c467a80ac81bac97c22a3a7a6
Steven Wu <stevenwu@Stevens-MacBook-Pro.local>
2024-03-23 13:26:47 -0400

    first push

---
+++ backend/api/models.py
@@ -0,0 +1,134 @@
+from django.db import models
+import uuid
+from datetime import datetime
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext as _
+from django.core.validators import MinValueValidator, MaxValueValidator
+from phonenumber_field.modelfields import PhoneNumberField
+
+
+class User(models.Model):
+    username = models.CharField(editable=False)
+    first_name = models.CharField(editable=False)
+    last_name = models.CharField(editable=False)
+    password = models.UUIDField(editable=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    email = models.EmailField(_("Email address"), unique=True, null=False)
+    bio = models.TextField(blank=True, null=True)
+    class_year = models.IntegerField(
+        _("Class year"),
+        validators=[
+            MinValueValidator(datetime.now().year),
+            MaxValueValidator(datetime.now().year + 4),
+        ],
+        blank=False,
+        null=False,
+    )
+    phone_number = PhoneNumberField(blank=True, null=True)
+    create_date = models.DateTimeField(_("Created at"), auto_now_add=True)
+    update_date = models.DateTimeField(_("Updated at"), auto_now=True)
+
+    def __str__(self):
+        return str(self.id)
+
+    class Meta:
+        verbose_name = "User"
+        verbose_name_plural = "Users"
+
+
+class Listing(models.Model):
+    STATUS_DENIED = 0
+    STATUS_ACCEPTED = 1
+    STATUS_PENDING = 2
+
+    STATUS_CHOICES = [
+        (STATUS_DENIED, "Denied"),
+        (STATUS_ACCEPTED, "Accepted"),
+        (STATUS_PENDING, "Pending"),
+    ]
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    listing_type = models.BooleanField(
+        _("Listing type"), default=False
+    )
+    title = models.CharField(_("Title"), max_length=255)
+    location = models.CharField(_("Location"), max_length=255)
+    description = models.TextField(_("Description"), blank=True, null=True)
+    form = models.BooleanField(
+        _("Form"), default=False
+    )
+    price = models.DecimalField(
+        _("Price"),
+        validators=[
+            MinValueValidator(0),
+        ],
+        max_digits=10,
+        decimal_places=2,
+        blank=True,
+        null=True,
+    )
+    status = models.IntegerField(choices=STATUS_CHOICES, default=STATUS_PENDING)
+    payment_method = models.CharField(max_length=255)
+    create_date = models.DateTimeField(_("Created at"), auto_now_add=True)
+    update_date = models.DateTimeField(_("Updated at"), auto_now=True)
+    category = models.CharField(_("Category"), max_length=255, blank=False, null=False)
+    condition = models.CharField(
+        _("Condition"), max_length=255, blank=False, null=False
+    )
+    clicked = models.IntegerField(
+        _("Number of interaction"),
+        validators=[MinValueValidator(0)],
+        blank=True,
+        null=False,
+    )
+
+    def __str__(self):
+        return self.title
+
+
+class Image(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    image = models.URLField(_("Image_URL"))
+    create_date = models.DateTimeField(_("Created at"), auto_now_add=True)
+    update_date = models.DateTimeField(_("Updated at"), auto_now=True)
+    listing_image = models.ForeignKey(Listing, on_delete=models.CASCADE)
+    profile_image = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Image {self.id}"
+
+
+class Transaction(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        validators=[
+            MinValueValidator(0),
+        ],
+    )
+    listing_id = models.ForeignKey(Listing, on_delete=models.CASCADE)
+    buyer_id = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="user_transactions", default=""
+    )
+    seller_id = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="seller_transactions"
+    )
+    time_posted = models.DateTimeField(auto_now_add=True)
+    time_sold = models.DateTimeField()
+
+
+class MyBids(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    listing_id = models.ForeignKey(Listing, on_delete=models.CASCADE)
+    offered_price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        validators=[
+            MinValueValidator(0),
+        ],
+    )
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    message = models.TextField(blank=True, null=True)
+
---
+++ backend/backend/settings.py
@@ -0,0 +1,93 @@
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = 'django-insecure-k18n^wptpkddmce0l1i8_@=q(f(9lr+w#y4$n*m7y!h#fsvtq2'
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+INSTALLED_APPS = [
+    'api',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'rest_framework'
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'backend.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'backend.wsgi.application'
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'db-postgresql-nyc3-49863',
+        'USER': 'doadmin',
+        'PASSWORD': 'AVNS_Bv5I6FXbNlV-B9nsI2-',
+        'HOST': 'db-postgresql-nyc3-49863-do-user-16116037-0.c.db.ondigitalocean.com',
+        'PORT': '25060'
+    }
+}
+
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_TZ = True
+
+
+STATIC_URL = 'static/'
+
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
